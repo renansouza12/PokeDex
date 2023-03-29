68056eeb882f4f4eb409648dc72bf37a36ff43d0
RenanSouza <renansouza0122@gmail.com>
2023-03-29 16:42:29 -0300

    validation case the button prev  be less than 1

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ const pokemonHeight = document.querySelector('.pokemon_height');
 const pokemonWeight = document.querySelector('.pokemon_weight');
 const pokemonAbility = document.querySelector('.pokemon_ability');
 const pokemonInfo = document.querySelector('.pokemon_informations');
+const arrows = document.querySelector('.arrows')
 const pokemonNext = document.querySelector('.arrow_next');
 const pokemonPrev = document.querySelector('.arrow_prev');
 
@@ -21,6 +22,8 @@ async function fetchPokemon(pokemon){
          pokemonImage.style.display = 'block';
          pokemonInfo.style.display = 'block';  
          pokemonId.style.fontSize = '15rem';
+         pokemonPrev.style.opacity = '1';
+         pokemonNext.style.transform = 'translateY(0)';
         return data;  
     }
     else{
@@ -28,6 +31,9 @@ async function fetchPokemon(pokemon){
         pokemonId.style.fontSize = '11rem';
         pokemonImage.style.display = 'none';
         pokemonInfo.style.display = 'none';  
+        pokemonPrev.style.opacity = '0';
+        pokemonNext.style.transform = 'translateY(-25rem)';
+        
     }
    
 }
@@ -43,9 +49,10 @@ async function showPokemon(pokemon){
     pokemonId.innerHTML = addZero(id);
     pokemonHeight.innerHTML = height;
     pokemonWeight.innerHTML = weight;
-
+    
     pokemonImage.src = data['sprites']['other']['home']['front_default'];
     pokemonAbility.innerHTML = data['abilities']['0']['ability']['name'];
+    
 
 }
 searchBox.addEventListener('submit',(e)=>{
@@ -68,8 +75,11 @@ pokemonNext.addEventListener('click',()=>{
     showPokemon(currentPokemon);
 })
 pokemonPrev.addEventListener('click',()=>{
-    currentPokemon --;
-    showPokemon(currentPokemon);
+    if(currentPokemon > 1){
+        currentPokemon --;
+        showPokemon(currentPokemon);
+    }
+  
 })
 
 function addZero(id){
