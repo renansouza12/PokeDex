2e2fc5f97577e844fd83ba8889fcd93d1d5b04cf
Renan Souza <renansouza0122@gmail.com>
2023-03-30 20:55:55 -0300

    validation if the hp attack denfense number are less than ten

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const searchBox = document.querySelector('.search_box');
-const icon =document.querySelector('.fa-magnifying-glass');
+const icon = document.querySelector('.fa-magnifying-glass');
 const iconSearch = document.querySelector('#icon_search');
 const iconRemove = document.querySelector('#icon_remove');
 const searchBtn = document.querySelector('.search_btn');
@@ -16,48 +16,53 @@ const pokemonNext = document.querySelector('.arrow_next');
 const pokemonPrev = document.querySelector('.arrow_prev');
 
 
-async function fetchPokemon(pokemon){
+async function fetchPokemon(pokemon) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    if(response.status == 200){
+    if (response.status == 200) {
         const data = await response.json();
-         pokemonImage.style.display = 'block';
-         pokemonInfo.style.display = 'block';  
-         pokemonId.style.fontSize = '15rem';
-         pokemonPrev.style.opacity = '1';
-         pokemonNext.style.transform = 'translateY(0)';
-        return data;  
+        pokemonImage.style.display = 'block';
+        pokemonInfo.style.display = 'block';
+        pokemonId.style.fontSize = '15rem';
+        pokemonPrev.style.opacity = '1';
+        pokemonNext.style.transform = 'translateY(0)';
+        return data;
     }
-    else{
+    else {
         pokemonId.innerHTML = 'Not Found 😴'
         pokemonId.style.fontSize = '9rem';
         pokemonImage.style.display = 'none';
-        pokemonInfo.style.display = 'none';  
+        pokemonInfo.style.display = 'none';
         pokemonPrev.style.opacity = '0';
         pokemonNext.style.transform = 'translateY(-5rem)';
-        
+
     }
-   
+
 }
 
 let currentPokemon = 1;
 
-async function showPokemon(pokemon){
+async function showPokemon(pokemon) {
     pokemonName.innerHTML = 'Loading...'
     const data = await fetchPokemon(pokemon);
-    const {name, id, height, weight}= data; 
+    const { name, id, height, weight } = data;
     pokemonName.innerHTML = name;
-    pokemonId.innerHTML = addZero(id); 
+    pokemonId.innerHTML = addZero(id);
     pokemonHeight.innerHTML = height;
     pokemonWeight.innerHTML = weight;
-    pokemonImage.src = data['sprites']['other']['home']['front_default'];    
-    pokemonAbility.innerHTML = data['abilities']['0']['ability']['name']; 
+    pokemonImage.src = data['sprites']['other']['home']['front_default'];
+    pokemonAbility.innerHTML = data['abilities']['0']['ability']['name'];
     currentPokemon = id;
 
-    document.querySelector('.hp_number').innerHTML = Math.floor(Math.random() * data.stats[0].base_stat + 1);
-    document.querySelector('.attack_number').innerHTML = Math.floor(Math.random() * data.stats[1].base_stat + 1);
-    document.querySelector('.defense_number').innerHTML = Math.floor(Math.random() * data.stats[2].base_stat + 1);
+
+    const hpNumber = Math.floor(Math.random() * data.stats[0].base_stat + 1);
+    const attackNumber = Math.floor(Math.random() * data.stats[1].base_stat + 1);
+    const defenseNumber = Math.floor(Math.random() * data.stats[2].base_stat + 1);
+
+    document.querySelector('.hp_number').innerHTML = hpNumber < 10 ? '0' + hpNumber : hpNumber;
+    document.querySelector('.attack_number').innerHTML = attackNumber < 10 ? '0' + attackNumber : attackNumber;
+    document.querySelector('.defense_number').innerHTML = defenseNumber < 10 ? '0' + defenseNumber : defenseNumber;
 }
-searchBox.addEventListener('submit',(e)=>{
+searchBox.addEventListener('submit', (e) => {
     e.preventDefault();
 
     showPokemon(inputPokemon.value.toLowerCase());
@@ -66,24 +71,24 @@ searchBox.addEventListener('submit',(e)=>{
 showPokemon(currentPokemon);
 
 
-searchBtn.addEventListener('click',()=>{
+searchBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-x');
     searchBox.classList.toggle('active');
     inputPokemon.classList.toggle('active');
     inputPokemon.value = '';
 })
-pokemonNext.addEventListener('click',()=>{
-    currentPokemon ++;
+pokemonNext.addEventListener('click', () => {
+    currentPokemon++;
     showPokemon(currentPokemon);
 })
-pokemonPrev.addEventListener('click',()=>{
-    if(currentPokemon > 1){
-        currentPokemon --;
+pokemonPrev.addEventListener('click', () => {
+    if (currentPokemon > 1) {
+        currentPokemon--;
         showPokemon(currentPokemon);
     }
-  
+
 })
 
-function addZero(id){
+function addZero(id) {
     return id < 10 ? "#00" + id : id < 100 ? "#0" + id : id >= 100 ? '#' + id : id;
 }
